4a4586bcf2e7e5b620895ac6b82430fd529cbd3d
Andrew Skotzko <andrew.skotzko@gmail.com>
2015-11-06 13:38:47 -0800

    stub out TestKit sample

---
+++ TestKit/src/Examples/ParentChild.cs
@@ -0,0 +1,35 @@
+﻿using Akka.Actor;
+using NUnit.Framework;
+using Akka.TestKit.NUnit;
+
+namespace TestKitSample.Examples
+{
+    public class ParentGreeter : ReceiveActor
+    {
+        public ParentGreeter()
+        {
+            Receive<string>(s => string.Equals(s, "greet parent"), s =>
+            {
+                Context.Parent.Tell("Hello parent!");
+            });
+        }
+    }
+
+    [TestFixture]
+    public class ParentGreeterSpecs : TestKit
+    {
+        [Test]
+        public void Parent_greeter_should_greet_parent()
+        {
+            Props greeterProps = Props.Create(() => new ParentGreeter());
+            // make greeter actor as child of TestActor
+            var greeter = ActorOfAsTestActorRef<ParentGreeter>(greeterProps, TestActor);
+            greeter.Tell("greet parent");
+
+            // TestActor captured any message that came back
+            ExpectMsg("Hello parent!");
+        }
+    }
+}
+
+
